5fb6958c5b91c6277d36a7e3de2253de02f74770
Letícia Moreira <138075524+leticiamjm@users.noreply.github.com>
2025-03-22 21:25:21 +0000

    hello-world-2

--- Basic/java-basic/src/App.java
+++ Basic/java-basic/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Meu primeiro código Java");
     }
 }
